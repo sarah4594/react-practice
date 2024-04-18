e6c772bf04788bfc6fc8def1e6915d0696502e1b
Sarah CJ <sarah4594@gmail.com>
2024-04-18 13:21:08 -0500

    SignUp login page and style

--- src/Pages/LoginSignUp.tsx
+++ src/Pages/LoginSignUp.tsx
@@ -2,8 +2,38 @@ import React from "react";
 
 const LoginSignUp = () => {
 	return (
-		<div>
-			<h1>Shop</h1>
+		<div className="loginSignup w-full h-[80vh] bg-gray-100 pt-24">
+			<div className="loginSignup-container w-[650px] h-[700px] bg-white m-auto py-10 px-16">
+				<h1 className="my-5 px-0">Sign Up</h1>
+				<div className="loginSignup-fields flex flex-col gap-8 mt-8">
+					<input
+						className="h-20 w-full pl-5 border border-gray-100 outline-none text-gray-600 text-lg"
+						type="text"
+						placeholder="Your Name"
+					/>
+					<input
+						className="h-20 w-full pl-5 border border-gray-100 outline-none text-gray-600 text-lg"
+						type="email"
+						placeholder="Email Address"
+					/>
+					<input
+						className="h-20 w-full pl-5 border border-gray-100 outline-none text-gray-600 text-lg"
+						type="password"
+						placeholder="Password"
+					/>
+				</div>
+				<button className="w-[500px] h-16 mt-8 text-white bg-red-500 text-2xl cursor-pointer">
+					Continue
+				</button>
+				<p className="loginSignup-login mt-5 text-gray-600 text-lg">
+					Already have an account?{" "}
+					<span className="text-red-500 font-semibold">Login here</span>
+				</p>
+				<div className="loginSignup-agree flex items-center mt-6 gap-5 text-gray-600 text-lg">
+					<input type="checkbox" name="" id="" />
+					<p>By continuing, I agree to the Terms and Policies</p>
+				</div>
+			</div>
 		</div>
 	);
 };
